7895f6d60cdd207a6a71ecd89f29df0f5862690a
Vinod Kumar <vinodrao835@gmail.com>
2025-02-16 13:12:33 +0530

    FIX: the issue of scan port timeout

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: process.env.PORT || 5173, // Use Render's provided port
+  },
   plugins: [react()],
-})
+});
